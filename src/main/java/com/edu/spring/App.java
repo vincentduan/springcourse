489acedf9b0448df23a620fe4255bddbd3a4faf0
vincent-duan <1621424380@qq.com>
2019-04-23 00:56:25 +0800

    update

--- src/main/java/com/edu/spring/App.java
+++ src/main/java/com/edu/spring/App.java
@@ -1,15 +1,17 @@
 package com.edu.spring;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
 
 public class App {
+
+
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.edu.spring");
-        System.out.println(context.getBean(User.class));
-        System.out.println(context.getBean("createUser"));
-        context.getBean(User.class).show();
-        context.getBean(Book.class).show();
-        context.getBean(Bank.class).show();
-        context.close();
+        ConfigurableApplicationContext context = SpringApplication.run(App2.class, args);
+        context.getBean(Runnable.class).run();
     }
 }
