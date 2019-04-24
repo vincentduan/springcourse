bc31ef64dc1095d1bf0a9c724db8f05112872aae
duandingyang <1621424380@qq.com>
2019-04-24 11:01:42 +0800

    update

--- src/main/java/com/edu/spring/App.java
+++ src/main/java/com/edu/spring/App.java
@@ -7,9 +7,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+        SpringApplication application = new SpringApplication(App.class);
+        application.setAdditionalProfiles("dev");
+//        application.setAdditionalProfiles("test");
+        ConfigurableApplicationContext context = application.run(args);
         System.out.println(context.getBean(TomcatProperties.class));
         System.out.println(context.getEnvironment().getProperty("springboot.name"));
+        System.out.println(context.getEnvironment().getProperty("jdbc.url"));
         context.close();
     }
 
