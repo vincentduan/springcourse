d914b10bb3e34023294c0a05f1f400c1f2fa66cf
vincent-duan <1621424380@qq.com>
2019-04-27 12:44:35 +0800

    update

--- src/main/java/com/edu/spring/springboot/App.java
+++ src/main/java/com/edu/spring/springboot/App.java
@@ -1,32 +1,48 @@
 package com.edu.spring.springboot;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.HashMap;
-import java.util.Map;
 
 /**
- * @SpringBootApplication默认扫描的包的路径是当前包以及子包下面的所有类，可以通过scanBasePackages修改这个扫描包的路径
+ * 运行流程
+ *  1. 判断是否是web环境
+ *  2. 加载所有classpath下面的META-INF/spring.factories,  ApplicationContextInitializerr
+ *  3. 加载所有classpath下面的META-INF/spring.factories, ApplicationListener
+ *  4. 推断main方法所在的类
+ *  5. 开始执行run方法
+ *  6. 设置java.awt.headless系统变量
+ *  7. 加载所有classpath下面的META-INF/spring.factories SpringApplicationRunListener
+ *  8. 执行所有SpringApplicationRunListener的started方法
+ *  9. 实例化ApplicationArguments对象
+ *  10. 创建Environment
+ *  11. 配置Environment，主要是把run方法的参数配置到Environment
+ *  12. 执行所有SpringApplicationRunListener的environment.prepared方法
+ *  13. 如果不是web环境，但是是web的Environment，则把web的Environment转换成标准的Environment
+ *  14. 输出Banner
+ *  15. 初始化applicationContext，如果是web环境，则实例化AnnotationConfigEmbeddedWebApplicationContext对象，否则实例化AnnotationConfigApplicationContext对象
+ *  16. 如果beanNameGenerator不为空，就把beanNameGenerator对象注入Context里面去
+ *  17. 回调所有的ApplicationContextInitializer方法
+ *  18. 执行所有SpringApplicationRunListener的contextPrepared方法
+ *  19. 依次往Spring容器中注入：ApplicationArguments, Banner
+ *  20. 加载所有的源到context里面去。
+ *  21. 执行所有SpringApplicationRunListener的contextLoaded方法
+ *  22. 执行context的refresh方法，并且调用context的registerShutdownHook方法
+ *  23. 回调，获取容器中所有的ApplicationRunner, CommandLineRunner接口，然后排序，依次调用
+ *  24. 执行所有SpringApplicationRunListener的finished的finished方法
  *
- * 自定义banner方法
- *  在classpath下放一个banner.txt的文件即可
  */
-@SpringBootApplication(scanBasePackages = "com.edu.spring", excludeName = "org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration")
+@SpringBootApplication
 public class App {
-    @Value(("${server.host:localhost}"))
-    private String serverHosts;
+
     public static void main(String[] args) {
+        // 实例化SpringApplication对象，然后调用run方法
         SpringApplication application = new SpringApplication(App.class);
-//        application.setBannerMode(Banner.Mode.OFF);
         ConfigurableApplicationContext context = application.run(args);
-        Map<String, Object> defaultProperties = new HashMap<>();
-        defaultProperties.put("server.host", "192.168.1.1");
-        application.setDefaultProperties(defaultProperties);
-        System.out.println(context.getEnvironment().getProperty("server.host","localhost2"));
-        System.out.println(context.getBean(App.class).serverHosts);
+        // 直接调用静态run方法
+        // ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
         context.close();
     }
+
 }
