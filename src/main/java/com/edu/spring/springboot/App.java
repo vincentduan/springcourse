e122f0f921c17e39e83ab4d70ca8921c5e8a0f54
duandingyang <1621424380@qq.com>
2019-05-23 16:22:41 +0800

    update

--- src/main/java/com/edu/spring/springboot/App.java
+++ src/main/java/com/edu/spring/springboot/App.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+@SpringBootApplication
+@EnableTransactionManagement
 public class App {
     public static void main(String[] args) {
         ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(App.class, args);
@@ -19,10 +22,15 @@ public class App {
             System.out.println(connection.getCatalog());
             connection.close();
             ProductDao bean = configurableApplicationContext.getBean(ProductDao.class);
-            bean.addProduct("123");
+            try {
+                bean.addTest("111", "222", "333", "444", "555");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        System.out.println(dataSource.getClass());
     }
 
 }
--- src/main/java/com/edu/spring/springboot/ProductDao.java
+++ src/main/java/com/edu/spring/springboot/ProductDao.java
@@ -3,6 +3,9 @@ package com.edu.spring.springboot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.FileNotFoundException;
 
 @Repository
 public class ProductDao {
@@ -13,6 +16,38 @@ public class ProductDao {
     public void addProduct(String id){
         String sql = "insert into test (id) values ("+ id + ")";
         jdbcTemplate.execute(sql);
+    }
+
+    /**
+     * roobackFor 设置对哪些异常进行回滚，默认是运行时异常
+     * @param ids
+     * @throws FileNotFoundException
+     */
+    @Transactional(rollbackFor = Exception.class, noRollbackFor = NullPointerException.class)
+    public void addProductBatch(String ...ids) throws Exception {
+        for(String id: ids){
+            String sql = "insert into test (id) values ("+ id + ")";
+            jdbcTemplate.execute(sql);
+            if("".equals("")) {
+                throw new NullPointerException();
+            }
+        }
+
+    }
+
+    public void addTest(String ...ids){
+        add(ids);
+    }
+
+    @Transactional()
+    private void add(String ...ids){
+        for(String id: ids){
+            String sql = "insert into test (id) values ("+ id + ")";
+            jdbcTemplate.execute(sql);
+            if("".equals("")) {
+                throw new NullPointerException();
+            }
+        }
     }
 
 }
