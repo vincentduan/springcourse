3f82f2feb7d6dc7ef5a22330ad1b762fd8fffe19
duandingyang <duandingyang@bjtu.edu.cn>
2019-06-01 15:26:55 +0800

    update

--- src/main/java/com/edu/spring/springboot/App.java
+++ src/main/java/com/edu/spring/springboot/App.java
@@ -1,34 +1,14 @@
 package com.edu.spring.springboot;
 
-import com.edu.spring.springboot.dao.UserDao;
-import com.edu.spring.springboot.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-/**
- * springboot默认的级别是info
- * 可以通过logging.level.*=debug 来设置，* 可以是包，也可以是某个类。
- * 日志级别有：trace，debug，info，warn，error，fatal，off
- * 日至级别off表示关闭日志
- *
- * logging.pattern.console配置控制台输出日志的pattern
- * logging.file.console配置文件输出日志的pattern
- *
- * 使用其他的日志组件的步骤
- * 1：排除掉默认的日志组件spring-boot-starter-logging
- * 2：加入新的日志组件依赖
- * 3：把相应的日志文件加到classpath下
- */
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
 
         ConfigurableApplicationContext run = SpringApplication.run(App.class, args);
-        run.getBean(UserDao.class).log();
-        run.getBean(UserService.class).log();
-        run.close();
-
     }
 
 }
